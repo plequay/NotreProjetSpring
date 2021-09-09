befe82ed7ce6badb146dd8cc98f369061c23dc01
Pierre <pi.lequay@gmail.com>
2021-09-09 16:04:59 +0200

    Console Service initialisation des donénes batiment et ressources

--- src/main/java/SopraAJC/NotreProjet/repositories/TransformationRessourceRepository.java
+++ src/main/java/SopraAJC/NotreProjet/repositories/TransformationRessourceRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import SopraAJC.NotreProjet.models.Transformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import SopraAJC.NotreProjet.models.Transformation;
 import SopraAJC.NotreProjet.models.TransformationRessource;
 
 public interface TransformationRessourceRepository extends JpaRepository <TransformationRessource, Integer> {
