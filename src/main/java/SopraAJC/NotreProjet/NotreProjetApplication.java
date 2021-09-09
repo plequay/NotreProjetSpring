df270185f2fbbf4ac347dcbe46bd4ef074261ea7
NicoK <nicolas.kroupa@gmail.com>
2021-09-09 10:04:28 +0200

    Init spring boot

---
+++ src/main/java/SopraAJC/NotreProjet/NotreProjetApplication.java
@@ -0,0 +1,13 @@
+package SopraAJC.NotreProjet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotreProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotreProjetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SopraAJC/NotreProjet/NotreProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package SopraAJC.NotreProjet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotreProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
