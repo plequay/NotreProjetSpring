a1c09b9db4da2f9084e976a9dad095551b17ea2c
Johan <johan.loison@yahoo.com>
2021-09-20 14:48:20 +0200

    crossorigin

--- src/main/java/SopraAJC/NotreProjet/restcontroller/SessionRessourceRestController.java
+++ src/main/java/SopraAJC/NotreProjet/restcontroller/SessionRessourceRestController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ import SopraAJC.NotreProjet.services.SessionService;
 
 @RestController
 @RequestMapping("/api/sessionressource")
+@CrossOrigin(origins = "*")
 public class SessionRessourceRestController {
 
 	@Autowired
