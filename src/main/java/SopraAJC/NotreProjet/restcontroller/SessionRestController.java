58bb986aca9761d25dba30d0ad658563a8be6605
Johan <johan.loison@yahoo.com>
2021-09-22 16:50:10 +0200

    ajout fonction delete session

--- src/main/java/SopraAJC/NotreProjet/restcontroller/SessionRestController.java
+++ src/main/java/SopraAJC/NotreProjet/restcontroller/SessionRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -113,6 +114,12 @@ public class SessionRestController {
         });
 
         return sessions;
+    }
+    
+    @DeleteMapping("/{idP}/[idC}")
+    @JsonView(JsonViews.SessionWithAll.class)
+    public void deleteSession(@PathVariable Integer idP, @PathVariable Integer idC) {
+    	 sessionRepository.delete(sessionRepository.findByPartieAndCompte(pRepo.findById(idP).get(), cRepo.findById(idC).get()).get());
     }
 
     
